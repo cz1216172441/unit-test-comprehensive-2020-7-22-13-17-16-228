6a445ba1d9b54c1f8a0ea20e5bfc9c2cbf16c3ed
CHENRO6 <chenro6@oocl.com>
2020-07-23 22:00:07 +0800

    [Guess Number TDD][Roy] feature: pass should_return_false_when_input_param_validate_given_one_value_less_than_0_at_least

--- src/main/java/example/InputValidator.java
+++ src/main/java/example/InputValidator.java
@@ -2,6 +2,19 @@ package example;
 
 public class InputValidator {
     public boolean inputParamValidate(int[] guessAnswer) {
-        return guessAnswer != null && guessAnswer.length == 4;
+        boolean isParamValid = false;
+        if (guessAnswer != null) {
+            isParamValid = guessAnswer.length == 4;
+            if (!isParamValid) {
+                return false;
+            }
+            for (int value: guessAnswer) {
+                if (value < 0) {
+                    isParamValid = false;
+                    break;
+                }
+            }
+        }
+        return isParamValid;
     }
 }

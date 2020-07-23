fbe1131167370b54f8629541ab3b3334a61f3441
CHENRO6 <chenro6@oocl.com>
2020-07-23 21:28:46 +0800

    [Guess Number TDD][Roy] test: should_return_false_when_input_param_validate_given_null

---
+++ src/test/java/example/InputValidatorTest.java
@@ -0,0 +1,18 @@
+package example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class InputValidatorTest {
+
+    @Test
+    void should_return_false_when_input_param_validate_given_null() {
+        // given
+        InputValidator inputValidator = new InputValidator();
+        int[] guessAnswer = null;
+        // when
+        boolean isParamValid = inputValidator.inputParamValidate();
+        // then
+        Assertions.assertFalse(isParamValid);
+    }
+}

93d0eb66b6e9b4d8da3929699f6988141f6cbb41
CHENRO6 <chenro6@oocl.com>
2020-07-23 20:18:29 +0800

    [Guess Number TDD][Roy] test: should_return_0A0B_when_guess_number_given_answer_1234_and_guess_answer_5678

--- src/test/java/example/GuessNumberTest.java
+++ src/test/java/example/GuessNumberTest.java
@@ -2,6 +2,8 @@ package example;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
 import static org.mockito.Mockito.*;
 
 public class GuessNumberTest {
@@ -17,5 +19,18 @@ public class GuessNumberTest {
         String actual = guessNumber.guess(guessAnswer);
         // then
         Assertions.assertEquals("4A0B", actual);
+    }
+
+    @Test
+    void should_return_0A0B_when_guess_number_given_answer_1234_and_guess_answer_5678() {
+        // given
+        int[] guessAnswer = {5, 6, 7, 8};
+        AnswerGenerator answerGenerator = mock(AnswerGenerator.class);
+        when(answerGenerator.generator()).thenReturn(new int[]{5, 6, 7, 8});
+        GuessNumber guessNumber = new GuessNumber(answerGenerator);
+        // when
+        String actual = guessNumber.guess(guessAnswer);
+        // then
+        Assertions.assertEquals("0A0B", actual);
     }
 }

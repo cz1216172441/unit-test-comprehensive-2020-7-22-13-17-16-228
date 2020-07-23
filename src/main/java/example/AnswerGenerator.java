ec843e653c06026a86e3a0c1611c4d19bea751ac
CHENRO6 <chenro6@oocl.com>
2020-07-23 22:40:41 +0800

    [Guess Number TDD][Roy] feature: pass should_return_array_and_not_repeated_value_when_generate

--- src/main/java/example/AnswerGenerator.java
+++ src/main/java/example/AnswerGenerator.java
@@ -1,14 +1,22 @@
 package example;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class AnswerGenerator {
 
     public int[] generator() {
-        int[] answer = new int[4];
+        int length = 4;
+        int[] answer = new int[length];
+        List<Integer> list = new ArrayList<>(length);
         Random random = new Random();
-        for (int i = 0; i < answer.length; i++) {
-            answer[i] = random.nextInt(10);
+        for (int i = 0; i < length; i++) {
+            Integer value = random.nextInt(10);
+            if (!list.contains(value)) {
+                list.add(value);
+                answer[i] = value;
+            }
         }
         return answer;
     }

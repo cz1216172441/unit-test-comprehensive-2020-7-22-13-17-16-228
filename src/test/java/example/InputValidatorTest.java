1a3db5af9939f8f136963eed2e5360e60c1a23bf
CHENRO6 <chenro6@oocl.com>
2020-07-23 21:51:34 +0800

    [Guess Number TDD][Roy] test: should_return_false_when_input_param_validate_given_one_value_less_than_0_at_least

--- src/test/java/example/InputValidatorTest.java
+++ src/test/java/example/InputValidatorTest.java
@@ -48,4 +48,15 @@ public class InputValidatorTest {
         // then
         Assertions.assertFalse(isParamValid);
     }
+
+    @Test
+    void should_return_false_when_input_param_validate_given_one_value_less_than_0_at_least() {
+        // given
+        InputValidator inputValidator = new InputValidator();
+        int[] guessAnswer = {-1, 2, 3, 4};
+        // when
+        boolean isParamValid = inputValidator.inputParamValidate(guessAnswer);
+        // then
+        Assertions.assertFalse(isParamValid);
+    }
 }

6238eec22c402ec27e43c67a0a6120afbd6564d6
CHENRO6 <chenro6@oocl.com>
2020-07-23 21:03:41 +0800

    [Guess Number TDD][Roy] refactor: extract guessNumber instance in class GuessNumberTest

--- src/test/java/example/GuessNumberTest.java
+++ src/test/java/example/GuessNumberTest.java
@@ -1,19 +1,31 @@
 package example;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 
 import static org.mockito.Mockito.*;
 
 public class GuessNumberTest {
 
+    @Mock
+    private AnswerGenerator answerGenerator;
+
+    @Mock
+    private GuessNumber guessNumber;
+
+    @BeforeEach
+    public void beforeAll() {
+        answerGenerator = mock(AnswerGenerator.class);
+        when(answerGenerator.generator()).thenReturn(new int[]{1, 2, 3, 4});
+        guessNumber = new GuessNumber(answerGenerator);
+    }
+
     @Test
     void should_return_4A0B_when_guess_number_given_answer_1234_and_guess_answer_1234() {
         // given
         int[] guessAnswer = {1, 2, 3, 4};
-        AnswerGenerator answerGenerator = mock(AnswerGenerator.class);
-        when(answerGenerator.generator()).thenReturn(new int[]{1, 2, 3, 4});
-        GuessNumber guessNumber = new GuessNumber(answerGenerator);
         // when
         String actual = guessNumber.guess(guessAnswer);
         // then
@@ -24,9 +36,6 @@ public class GuessNumberTest {
     void should_return_0A0B_when_guess_number_given_answer_1234_and_guess_answer_5678() {
         // given
         int[] guessAnswer = {5, 6, 7, 8};
-        AnswerGenerator answerGenerator = mock(AnswerGenerator.class);
-        when(answerGenerator.generator()).thenReturn(new int[]{1, 2, 3, 4});
-        GuessNumber guessNumber = new GuessNumber(answerGenerator);
         // when
         String actual = guessNumber.guess(guessAnswer);
         // then
@@ -37,9 +46,6 @@ public class GuessNumberTest {
     void should_return_2A0B_when_guess_number_given_answer_1234_and_guess_answer_1256() {
         // given
         int[] guessAnswer = {1, 2, 5, 6};
-        AnswerGenerator answerGenerator = mock(AnswerGenerator.class);
-        when(answerGenerator.generator()).thenReturn(new int[]{1, 2, 3, 4});
-        GuessNumber guessNumber = new GuessNumber(answerGenerator);
         // when
         String actual = guessNumber.guess(guessAnswer);
         // then
@@ -50,12 +56,10 @@ public class GuessNumberTest {
     void should_return_0A2B_when_guess_number_given_answer_1234_and_guess_answer_2356() {
         // given
         int[] guessAnswer = {2, 3, 5, 6};
-        AnswerGenerator answerGenerator = mock(AnswerGenerator.class);
-        when(answerGenerator.generator()).thenReturn(new int[]{1, 2, 3, 4});
-        GuessNumber guessNumber = new GuessNumber(answerGenerator);
         // when
         String actual = guessNumber.guess(guessAnswer);
         // then
         Assertions.assertEquals("0A2B", actual);
     }
+
 }

32b35174d556c7ff4862a6441e5d75bfed324942
CHENRO6 <chenro6@oocl.com>
2020-07-23 20:09:58 +0800

    [Guess Number TDD][Roy] feature: pass should_return_4A0B_when_guess_number_given_answer_1234_and_guess_answer_1234

--- src/main/java/example/GuessNumber.java
+++ src/main/java/example/GuessNumber.java
@@ -4,11 +4,21 @@ public class GuessNumber {
 
     private final int[] answer;
 
-    public GuessNumber() {
-        answer = new AnswerGenerator().generator();
+    public GuessNumber(AnswerGenerator answerGenerator) {
+        this.answer = answerGenerator.generator();
     }
 
     public String guess(int[] guessAnswer) {
+        boolean flag = true;
+        for (int i = 0; i < answer.length; i++) {
+            if (answer[i] != guessAnswer[i]) {
+                flag = false;
+                break;
+            }
+        }
+        if (flag) {
+            return "4A0B";
+        }
         return null;
     }
 }
--- src/test/java/example/GuessNumberTest.java
+++ src/test/java/example/GuessNumberTest.java
@@ -9,10 +9,10 @@ public class GuessNumberTest {
     @Test
     void should_return_4A0B_when_guess_number_given_answer_1234_and_guess_answer_1234() {
         // given
-        GuessNumber guessNumber = new GuessNumber();
         int[] guessAnswer = {1, 2, 3, 4};
         AnswerGenerator answerGenerator = mock(AnswerGenerator.class);
         when(answerGenerator.generator()).thenReturn(new int[]{1, 2, 3, 4});
+        GuessNumber guessNumber = new GuessNumber(answerGenerator);
         // when
         String actual = guessNumber.guess(guessAnswer);
         // then

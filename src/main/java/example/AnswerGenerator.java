1ec08ae324c7bbd614769327c1e2deaf459990ec
roy <1216172441@qq.com>
2020-08-01 23:08:23 +0800

    [Guess Number TDD][Roy] refactor: use IntStream to generate a 0-9 array and extract the answer constant

--- src/main/java/example/AnswerGenerator.java
+++ src/main/java/example/AnswerGenerator.java
@@ -1,16 +1,23 @@
 package example;
 
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class AnswerGenerator {
 
+    public static final int ANSWER_LOWER_BOUND = 0;
+    public static final int ANSWER_UPPER_BOUND = 9;
+    public static final int ANSWER_LIMITED_SIZE = 4;
+
     public int[] generate() {
-        List<Integer> source = Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+        List<Integer> source = IntStream.range(ANSWER_LOWER_BOUND, ANSWER_UPPER_BOUND + 1)
+                .boxed()
+                .collect(Collectors.toList());
         Collections.shuffle(source);
         return source.stream()
-                .limit(4)
+                .limit(ANSWER_LIMITED_SIZE)
                 .mapToInt(Integer::valueOf)
                 .toArray();
     }

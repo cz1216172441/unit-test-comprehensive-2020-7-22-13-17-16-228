0a7260ecf6256d600b96d4cac4d154381c19d12d
roy <1216172441@qq.com>
2020-08-01 23:55:33 +0800

    [Guess Number TDD][Roy] test: should_throw_number_format_exception_when_convert_given_a123

--- src/test/java/example/InputConverterTest.java
+++ src/test/java/example/InputConverterTest.java
@@ -1,6 +1,5 @@
 package example;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -15,5 +14,14 @@ public class InputConverterTest {
         int[] actual = InputConverter.convert(input);
         // then
         assertArrayEquals(new int[]{1, 2, 3, 4}, actual);
+    }
+
+    @Test
+    void should_throw_number_format_exception_when_convert_given_a123() {
+        // given
+        String input = "a 1 2 3";
+        // when
+        // then
+        assertThrows(NumberFormatException.class, () -> InputConverter.convert(input));
     }
 }

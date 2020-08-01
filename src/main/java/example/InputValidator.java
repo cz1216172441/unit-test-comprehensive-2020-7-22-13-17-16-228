ff81f51cab25f99469f49013b7c60bb97739dad9
roy <1216172441@qq.com>
2020-08-01 17:31:18 +0800

    [Guess Number TDD][Roy] feature: pass should_return_false_when_input_param_validate_given_1123

--- src/main/java/example/InputValidator.java
+++ src/main/java/example/InputValidator.java
@@ -7,9 +7,10 @@ public class InputValidator {
     public boolean inputParamValidate(int[] guessAnswer) {
         if (guessAnswer != null && guessAnswer.length == 4) {
             long count = Arrays.stream(guessAnswer)
-                    .filter(val -> val < 0 || val > 9)
+                    .filter(val -> val >= 0 && val <= 9)
+                    .distinct()
                     .count();
-            return count == 0;
+            return count == 4;
         }
         return false;
     }

061d651154c4fbdd506fb472a03187402483e5ce
roy <1216172441@qq.com>
2020-08-02 11:33:42 +0800

    [Guess Number TDD][Roy] test: should_output_game_won_when_play_given_times_5_and_final_result_is_4A0B

--- src/test/java/example/GuessApplicationTest.java
+++ src/test/java/example/GuessApplicationTest.java
@@ -27,14 +27,11 @@ public class GuessApplicationTest {
     @Mock
     private InputValidator inputValidator;
 
-    private PrintStream outStream;
-
     private ByteArrayOutputStream outContent;
 
     @BeforeEach
     public void setUp() {
         outContent = new ByteArrayOutputStream();
-        outStream = System.out;
         System.setOut(new PrintStream(outContent));
     }
 
@@ -62,11 +59,8 @@ public class GuessApplicationTest {
     void should_output_game_won_when_play_given_4A0B() {
         // given
         String input = "1 2 3 4";
-        int[] actualAnswer = new int[]{1, 2, 3, 4};
         // when
-        try (
-                MockedStatic<InputUnit> mocked = mockStatic(InputUnit.class)
-        ) {
+        try (MockedStatic<InputUnit> mocked = mockStatic(InputUnit.class)) {
             mocked.when(InputUnit::getInput).thenReturn(input);
             when(inputValidator.inputParamValidate(any())).thenReturn(true);
             when(guessNumber.guess(any())).thenReturn("4A0B");
@@ -77,6 +71,51 @@ public class GuessApplicationTest {
                     () -> assertEquals("Guess Game Start...", output[0]),
                     () -> assertEquals("please enter 4 numbers (0-9, no repeated, separated by spaces): ", output[1]),
                     () -> assertEquals("Game Won!!!", output[2])
+            );
+        }
+    }
+
+    @Test
+    void should_output_game_won_when_play_given_times_5_and_final_result_is_4A0B() {
+        // given
+        String input1 = "1 2 3 0";
+        String input2 = "1 2 3 4";
+        String result1 = "3A0B";
+        String result2 = "4A0B";
+        // when
+        try (MockedStatic<InputUnit> mocked = mockStatic(InputUnit.class)) {
+            mocked.when(InputUnit::getInput)
+                    .thenReturn(input1)
+                    .thenReturn(input1)
+                    .thenReturn(input1)
+                    .thenReturn(input1)
+                    .thenReturn(input1)
+                    .thenReturn(input2);
+            when(inputValidator.inputParamValidate(any())).thenReturn(true)
+                .thenReturn(true)
+                .thenReturn(true)
+                .thenReturn(true)
+                .thenReturn(true)
+                .thenReturn(true);
+            when(guessNumber.guess(any())).thenReturn(result1)
+                .thenReturn(result1)
+                .thenReturn(result1)
+                .thenReturn(result1)
+                .thenReturn(result1)
+                .thenReturn(result2);
+            guessApplication.play();
+            // then
+            String[] output = outContent.toString().split("\n");
+            assertAll(
+                    () -> assertEquals("Guess Game Start...", output[0]),
+                    () -> assertEquals("please enter 4 numbers (0-9, no repeated, separated by spaces): ", output[1]),
+                    () -> assertEquals("3A0B\n", output[2]),
+                    () -> assertEquals("3A0B\n", output[3]),
+                    () -> assertEquals("3A0B\n", output[4]),
+                    () -> assertEquals("3A0B\n", output[5]),
+                    () -> assertEquals("3A0B\n", output[6]),
+                    () -> assertEquals("4A0B\n", output[7]),
+                    () -> assertEquals("Game Won!!!", output[8])
             );
         }
     }
